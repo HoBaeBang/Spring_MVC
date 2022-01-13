7a7b7e0f1a3ada3ed0f45086de81c2ef8ca10d09
hobae <bhb0407@naver.com>
2022-01-13 19:25:55 +0900

    기본적인 서블릿을 이용한 통신

--- servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -11,16 +11,15 @@ import java.util.Enumeration;
 /**
  * 1. 파라미터 전송 기능
  * http://localhost:8080/request-param?username=hello&&age=20
- *
  */
-@WebServlet(name = "RequestParamServlet" , urlPatterns = "/request-param")
+@WebServlet(name = "RequestParamServlet", urlPatterns = "/request-param")
 public class RequestParamServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("[전체 파라미터 조회] - start");
         request.getParameterNames().asIterator()
-                        .forEachRemaining(paramName-> System.out.println(paramName + "=" + request.getParameter(paramName)));
+                .forEachRemaining(paramName -> System.out.println(paramName + "=" + request.getParameter(paramName)));
 
 
         System.out.println("[전체 파라미터 조회] - end");
@@ -37,7 +36,7 @@ public class RequestParamServlet extends HttpServlet {
 
         System.out.println("[이름이 같은 복수 파라미터 조회]");
         String[] usernames = request.getParameterValues("username");
-        for ( String name: usernames) {
+        for (String name : usernames) {
             System.out.println("name = " + name);
         }
 
