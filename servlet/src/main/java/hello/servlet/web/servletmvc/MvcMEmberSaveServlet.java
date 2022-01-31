a15187cc1ab55a4e8b0bc1c4de2ad775027e8ea8
hobae <bhb0407@naver.com>
2022-01-31 22:34:55 +0900

    멤버를 저장하기위한 서블릿

--- servlet/src/main/java/hello/servlet/web/servletmvc/MvcMEmberSaveServlet.java
+++ servlet/src/main/java/hello/servlet/web/servletmvc/MvcMEmberSaveServlet.java
@@ -1,7 +1,9 @@
 package hello.servlet.web.servletmvc;
 
+import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,8 +19,19 @@ public class MvcMEmberSaveServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
 
+        Member member = new Member(username,age);
 
+        memberRepository.save(member);
+
+        //Model에 데이터를 보관한다.
+        request.setAttribute("member", member);
+
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
     }
 
 }
